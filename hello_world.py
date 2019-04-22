cf65151e1b0f6debf9f7ce0c0f7026d4792c4ad8
Your Name <you@example.com>
2019-04-22 17:51:40 +0900

    first commit

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from bottle import route, run, Bottle, template
+
+# オブジェクト指向的な記述
+# app = Bottle()
+
+# @app.route("/hello")
+# def hello():
+#     return "Hello World"
+
+# # ビルトインのWebサーバが起動
+# run(app, host="localhost", port=8000)
+
+
+
+# ルートがデフォルトアプリケーションに複数追加
+# 
+@route("/")
+@route("/hello/<name>")
+# def greet(name='Stranger'):
+#     return template('Hello {{name}}, how are you?', name=name)
+def hello():
+    return "hello world"
+
+run(host="local_host", port=8000)
+
---
+++ index.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from bottle import route, run, template
+
+# @route('/hello/<name>')
+# def index(name):
+#     return template('<b>Hello {{name}}</b>!', name=name)
+
+# run(host='localhost', port=8000)
+
+
+@route("/object/<id:int>")
+def callback(id):
+    assert isinstance(id, int)
+    return "hello world"
+
+run(host="localhost", port=8000)
